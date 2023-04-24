8d0ea9c14676ebb6ccd89ed21e0348b873c82fe5
ZEBRA <luisguilherme.zebra@gmail.com>
2023-04-24 18:58:34 -0300

    go

--- INICIOServidor.bat
+++ INICIOServidor.bat
@@ -1,5 +1,11 @@
-FOR /F "tokens=2 delims=:" %%G IN ('ipconfig ^| find "IPv4 Address"') DO SET "MY_IP=%%G"
-SETLOCAL EnableDelayedExpansion
+for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /i "IPv4 Address.*$"') do (
+    SET "MY_IP=%%i"
+    goto :exitloop
+)
+:exitloop
+echo %MY_IP%
+type src\.env | find /v "LOCALHOST=" > temp & move /y temp src\.env
+echo LOCALHOST='%MY_IP%'>> src\.env
 SET "LOCALHOST=!MY_IP!"
 SET "APIPORT=3001"
 docker pull node:18.12.0-alpine

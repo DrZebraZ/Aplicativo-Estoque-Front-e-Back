3264e35282f1c9eb8d143ed38a8fd905cee19b5b
ZEBRA <luisguilherme.zebra@gmail.com>
2023-04-24 17:05:21 -0300

    bats

---
+++ INICIOServidor.bat
@@ -0,0 +1,10 @@
+FOR /F "tokens=2 delims=:" %%G IN ('ipconfig ^| find "IPv4 Address"') DO SET "MY_IP=%%G"
+SETLOCAL EnableDelayedExpansion
+SET "LOCALHOST=!MY_IP!"
+SET "APIPORT=3001"
+docker pull node:18.12.0-alpine
+docker pull mysql
+docker build -t gera-estoque -f .\src\Dockerfile .
+docker-compose up
+timeout 10
+start "" "INICIOUsuario.bat"

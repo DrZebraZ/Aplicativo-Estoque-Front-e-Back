ab0bb9e144724da9b236e0ee8d9d2a720799baf2
Luis Zebra <luis.andres@smartnx.com>
2023-07-05 02:17:25 -0300

    mint

--- installerMint.sh
+++ installerMint.sh
@@ -1,1 +1,26 @@
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y remove docker docker-engine docker.io containerd runc
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+cat /etc/apt/sources.list.d/docker.list
+
+sudo apt update
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker $USER
+newgrp docker
+
 cp ./app/.env.copy ./app/.env
+cp api/src/.env.copy api/src/.env
+sudo apt-get install net-tools
+sudo apt-get install coreutils
+
+
+
+
